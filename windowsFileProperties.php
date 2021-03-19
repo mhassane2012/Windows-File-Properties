64a0c313e0bc834aebeefda04e98a9e51eda6a0b
Hassane Moussa <mhassane2012@gmail.com>
2021-03-19 09:42:43 +0100

    Update windowsFileProperties.php
    Update get() Method

--- windowsFileProperties.php
+++ windowsFileProperties.php
@@ -6,7 +6,12 @@
  * 
  * Before used this class, below the prerequises 
  * 
- * PHP 5 or newer
+ *  See COM Requirements:
+ *  	COM functions are only available for the Windows version of PHP.
+ * 		.Net support requires PHP 5 and the .Net runtime.
+ *  	Enable the module COM and .NET extension in PHP :: extension=php_com_dotnet.dll
+ *  PHP 5 or newer
+ *
  * @author Hassane Moussa <mhassane2012@gmail.com>
  * @From Africa / Niamey-Niger
  * @License GPL
@@ -23,16 +28,23 @@
     public function __construct($file_path)
     {
 		$this->file_path = $file_path;
-	  
-		print "----PHP--Class--WindowsFileProperties-------------"."<br><br>";
-		print "File Name = ".$this->getRealFileName($this->file_path)."<br>";
-		print "File Version = ".$this->getRealFileVersion($this->file_path)."<br>";
-		print "File Size = ".$this->getRealFileSize($this->file_path)." octets<br>";
-		print "File Type = ".$this->getRealFileType($this->file_path)."<br>";
-		print "File Extension = ".$this->getRealFileExtension($this->file_path)."<br>";
-		print "File Path = ".$this->getRealFilePath($this->file_path)."<br>";
 		 
     }
+	/**
+    *  Print Stdout to html
+    */
+	function getStdoutHtml()
+	{
+		$msg= "----PHP--Class--WindowsFileProperties-------------"."<br><br>";
+		$msg.= "File Name = ".$this->getRealFileName()."<br>";
+		$msg.= "File Version = ".$this->getRealFileVersion()."<br>";
+		$msg.= "File Size = ".$this->getRealFileSize()." octets<br>";
+		$msg.= "File Type = ".$this->getRealFileType()."<br>";
+		$msg.= "File Extension = ".$this->getRealFileExtension()."<br>";
+		$msg.= "File Path = ".$this->getRealFilePath()."<br>";
+		
+		return $msg;
+	}
 	/**
     *  create File System objet(FSO) instance
     */
@@ -43,50 +55,50 @@
 	/**
     *  File Size octets
     */
-	function getRealFileSize($file_path)
+	function getRealFileSize()
 	{
 		$this->fs = $this->getFileSystOb();
-		return number_format($this->fs->GetFile($file_path)->Size,0,""," ");
+		return number_format($this->fs->GetFile($this->file_path)->Size,0,""," ");
 	}
 	/**
     *  File Type
     */
-	function getRealFileType($file_path)
+	function getRealFileType()
 	{
 		$this->fs = $this->getFileSystOb();
-		return $this->fs->GetFile($file_path)->Type;
+		return $this->fs->GetFile($this->file_path)->Type;
 	}
 	/**
     *  File Name
     */
-	function getRealFileName($file_path)
+	function getRealFileName()
 	{
 		$this->fs = $this->getFileSystOb();
-		return $this->fs->GetFile($file_path)->Name;
+		return $this->fs->GetFile($this->file_path)->Name;
 	}
 	/**
     *  File Path
     */
-	function getRealFilePath($file_path)
+	function getRealFilePath()
 	{
 		$this->fs = $this->getFileSystOb();
-		return $this->fs->GetFile($file_path)->Path;
+		return $this->fs->GetFile($this->file_path)->Path;
 	}
 	/**
     *  File Extension (.exe,...)
     */
-	function getRealFileExtension($file_path)
+	function getRealFileExtension()
 	{
 		$this->fs = $this->getFileSystOb();
-		return $this->fs->GetExtensionName($file_path);
+		return $this->fs->GetExtensionName($this->file_path);
 	}
 	/**
     *  File Version
     */
-	function getRealFileVersion($file_path)
+	function getRealFileVersion()
 	{
 		$this->fs = $this->getFileSystOb();
-		return $this->fs->GetFileVersion(realpath($file_path));
+		return $this->fs->GetFileVersion(realpath($this->file_path));
 	}
  
  
