e6032143aa38631eaf01dc05d30b4e08da6a70e1
Hassane Moussa <mhassane2012@gmail.com>
2021-03-19 09:44:07 +0100

    Update index.php
    many examples of Call methods

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <?php 
-/***************************************************************************************
+/****************************************************************************************************************
 							Main Using  Class WindowsFileProperties
-***************************************************************************************/
+							
+								Below the prerequises
+							See COM Requirements:
+								COM functions are only available for the Windows version of PHP.
+									.Net support requires PHP 5 and the .Net runtime.
+								Enable the module COM and .NET extension in PHP :: extension=php_com_dotnet.dll
+							PHP 5 or newer
+****************************************************************************************************************/
  require('windowsFileProperties.php');
  
-	$path = "D:\\wamp\\www\\windowsFileProperties\\TeamViewer10_Setup_fr.exe";
+	$path = "D:\\wamp\\www\\windowsFileProperties\\TeamViewer10_Setup_fr.exe"; // For Example
 
-	new windowsFileProperties($path);
-
+	$objectCom = new windowsFileProperties($path);  // create objet
 	
+	echo $objectCom->getStdoutHtml(); // Example to display all get() method functions in HTML
+	
+echo "<br>/*********************Example of Call all functions*************************/<br><br>";
+	
+				 echo $objectCom->getRealFileName()."<br>";  // File Name
+				 echo $objectCom->getRealFileVersion()."<br>"; // File Version
+				 echo $objectCom->getRealFileSize()."<br>";   // File Size
+				 echo $objectCom->getRealFileType()."<br>";   // File Type
+				 echo $objectCom->getRealFileExtension()."<br>"; // File Extension
+				 echo $objectCom->getRealFilePath();  // File Path
    
 ?>
